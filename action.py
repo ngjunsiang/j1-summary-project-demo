5dd516ab063103e210187b9d8d78d720a3c5a2f7
JS Ng <45561895+ngjunsiang@users.noreply.github.com>
2024-08-01 08:06:44 +0000

    Added Game class interface, action module and base class

---
+++ action.py
@@ -0,0 +1,8 @@
+"""action.py
+
+Classes represent actions taken by the player
+"""
+
+class Action:
+    """Base class for all actions."""
+
--- mud.py
+++ mud.py
@@ -1,2 +1,46 @@
+"""mud.py
+
+Main module for the game.
+"""
+
+import action
+
+def epilogue():
+    print("Thanks for playing!")
+    print("(Stay tuned for more.)")
+
+def welcome():
+    print("TODO: Add welcome message")
+
+
 class Game:
-    pass
+    def __init__(self):
+        pass
+
+    def add_player(self, player):
+        """Add player to the game"""
+        raise NotImplementedError
+
+    def execute(self, actions: list[Action]) -> None:
+        """Carry out list of actions"""
+        raise NotImplementedError
+
+    def get_options(self) -> list[str]:
+        """Return list of player options"""
+        options = []
+        return options
+
+    def get_actions(self, choice: str) -> list[Action]:
+        """Return list of actions based on player choice"""
+        actions = []
+        return actions
+
+    def is_gameover(self) -> bool:
+        # TODO: Determine gameover condition
+        return False
+
+    def status(self) -> dict:
+        """Return game status as a dict"""
+        status = {}
+        return status
+    
